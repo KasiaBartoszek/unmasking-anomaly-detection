11baa2832e6aa4069cab9e3e767e3ed2e53ecb1e
KasiaBartoszek <kbartoszek2006@gmail.com>
2021-06-25 23:16:54 +0200

    Added results presentation

--- main.py
+++ main.py
@@ -3,19 +3,36 @@ from feature_extractor import FeatureExtractor
 import argparse
 from unmasking_algorithm import UnmaskingAlgorithm
 import cv2
+import matplotlib.pyplot as plt
 
 def run(args):
     frame_loader = FrameLoader()
     feature_extractor = FeatureExtractor()
     frames = frame_loader.load_frames_from_dir(args.path, 'tif')
     normal_frames, abnormal_frames = frame_loader.divide_frames_into_batches(frames)
-    appearance_normal, appearance_abnormal = feature_extractor.extract_appearance_features(normal_frames, abnormal_frames)
-    unmasking_algorithm = UnmaskingAlgorithm(normal_frames, abnormal_frames, appearance_normal, appearance_abnormal)
+    #appearance_normal, appearance_abnormal = feature_extractor.extract_appearance_features(normal_frames, abnormal_frames)
+    unmasking_algorithm = UnmaskingAlgorithm(normal_frames, abnormal_frames, [], [])
     results = unmasking_algorithm.run(args.k, args.m)
     final_scores = unmasking_algorithm.get_final_scores(frames)
 
-    cv2.imshow(f'{final_scores[20]}', frames[20])
-    cv2.waitKey()
+    normal_values = []
+    abnormal_values = []
+    normal_averages = []
+    abnormal_averages = []
+
+    for index in range(len(results)):
+        for i in range(len(results[index])):
+            normal_values.append(results[index][i][0])
+            abnormal_values.append(results[index][i][1])
+        normal_averages.append(sum(normal_values) / len(normal_values))
+        abnormal_averages.append(sum(abnormal_values) / len(abnormal_values))
+
+    plt.plot(normal_averages)
+    plt.plot(abnormal_averages)
+    plt.show()
+
+    #cv2.imshow(f'{final_scores[20]}', frames[20])
+    #cv2.waitKey()
 
 
 if __name__ == '__main__':
